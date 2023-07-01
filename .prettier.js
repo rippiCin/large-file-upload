5339ad0122e2cb93a9d8113b1038df12a06f4ed3
Rippi <694404609@qq.com>
2023-07-01 19:12:25 +0800

    feat: init eslint & prettier & husky

---
+++ .prettier.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true, // 句末加分号
+  singleQuote: true, // 使用单引号
+  bracketSpacing: true, // 对象、数组加空格
+  jsxBracketSameLine: true, // jsx > 是否另起一行
+  arrowParens: "always", // 函数参数是否有括号
+  insertPragma: false, //在已被prettier格式化的文件顶部加上标注
+  htmlWhitespaceSensitivity: "ignore", //对HTML全局空白不敏感
+};
