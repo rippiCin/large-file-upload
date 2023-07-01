73eaf396055df57ca7b1ba21021013d7e885167c
Rippi <694404609@qq.com>
2023-07-01 19:39:26 +0800

    feat: add eslint env: node

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,9 +1,9 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((_, response) => {
-  response.end('hello Http server');
+  response.end("hello Http server");
 });
 
 server.listen(9000, () => {
-  console.log('服务已经启动了...');
-})
+  console.log("服务已经启动了...");
+});
