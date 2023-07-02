c687d96a8b0ee3e899213e0dabb4cc2a59020e9e
Rippi <694404609@qq.com>
2023-07-02 12:00:27 +0800

    fix: prettier setting filename

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,7 +5,8 @@ module.exports = {
   singleQuote: true, // 使用单引号
   bracketSpacing: true, // 对象、数组加空格
   jsxBracketSameLine: true, // jsx > 是否另起一行
-  arrowParens: "always", // 函数参数是否有括号
+  arrowParens: 'always', // 函数参数是否有括号
   insertPragma: false, //在已被prettier格式化的文件顶部加上标注
-  htmlWhitespaceSensitivity: "ignore", //对HTML全局空白不敏感
+  htmlWhitespaceSensitivity: 'ignore', //对HTML全局空白不敏感
+  embeddedLanguageFormatting: 'auto', //对引用代码进行格式化
 };
